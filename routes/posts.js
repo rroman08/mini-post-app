93dea7543a8c9db691a6408e3b8fb55208e47de0
Roman Rudolf <roman_rudolf@icloud.com>
2024-10-17 21:50:16 +0100

    Added post user in posts

--- routes/posts.js
+++ routes/posts.js
@@ -1,10 +1,25 @@
 
 const express = require('express')
-
 const router = express.Router()
 
+const Post = require('../models/Post')
+
 router.post('/', async (req, res) => {
-    console.log(req.body)
+    const postData = new Post({
+        user: req.body.user,
+        title: req.body.title,
+        text: req.body.text,
+        hashtag: req.body.hashtag,
+        location: req.body.location,
+        url: req.body.url
+    })
+
+    try {
+        const postToSave = await postData.save()
+        res.send(postToSave)
+    } catch(err) {
+        res.send({message: err})
+    }
 })
 
 module.exports = router
