b69cc10644567726b47169eda9d14e658b236476
Roman Rudolf <roman_rudolf@icloud.com>
2024-10-17 20:53:52 +0100

    Connected to MongoDB

--- app.js
+++ app.js
@@ -2,16 +2,16 @@
 const epxress = require('express')
 const app = epxress()
 
+const mongoose = require('mongoose')
+require('dotenv/config')
+
 const bodyParser = require('body-parser')
 const postRoute = require('./routes/posts')
 
 app.use(bodyParser.json())
 app.use('/posts', postRoute)
 
-app.get('/', (req, res) => {
-    res.send('hello')
-})
-
+mongoose.connect(process.env.DB_CONNECTOR)
 
 app.listen(3000, () => {
     console.log('Server is up and running ...')
